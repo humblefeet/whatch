134c3b6fb65d1b043c1882286fee39b85d602064
chauncy-n <eldoon@comcast.net>
2018-11-02 15:23:18 -0700

    no changes made commiting to pul from upstream

--- models/Image.js
+++ models/Image.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 require('mongoose-type-url');
 
 var imageSchema = new mongoose.Schema({
-    genres: [{ type: mongoose.Schema.Types.ObjectId, ref:'Genre'}],
-    url:  {type: mongoose.SchemaTypes.Url, required: true}
+    genres:[{ type: mongoose.Schema.Types.ObjectId, ref:'Genre'}],
+    url: {type: mongoose.SchemaTypes.Url, required: true}
 });
 
 module.exports = mongoose.model('Image', imageSchema);
