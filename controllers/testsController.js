9b8807120ba8e8c6d9d9be01ddb08b807d9d4dbd
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-11-03 16:36:27 -0700

    Added testcontroller, test route, test view

---
+++ controllers/testsController.js
@@ -0,0 +1,7 @@
+var Image = require('../models/Image');
+
+module.exports = {
+    showTest: function(req,res,next){
+        res.render('recTests/test1');
+    }
+}
---
+++ routes/test.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var test = require('../controllers/testsController');
+
+
+
+router.get('/', test.showTest );
+
+module.exports = router;
