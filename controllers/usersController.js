b396cb2eba4c811a78fd2dd2c4f9018d748a511b
Sarah Stensberg <ladystensberg@gmail.com>
2018-11-03 16:22:20 -0700

    commented out the image history

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,12 +5,10 @@ module.exports = {
 	profile: function(req, res, next) {
 		User.findById(req.params.id, function(err, user) {
 			if (err) return next(err);
-			Image.find({}, function(err, image) {
-				user.histories.push({
-					images: [image]
-				})
-			})
-			user.save();
+			// Image.find({}, function(err, image) {
+			// 	user.histories.push(image._id)
+			// })
+			// user.save();
 			res.render('users/profile', {user})
 		})
 	}
