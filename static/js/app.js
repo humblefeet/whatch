7759d46e5d77d0c57551756490e9386c94f4f527
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-11-04 15:03:12 -0800

    created click events on test page in app.js. Adjusted css.

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+var testImages =  document.querySelectorAll('.tests');  
+var genresSelected = [];
+var clickCheck = [];
+var clickCount = 0;
+
+function imageSelect(){
+    testImages.forEach(function(image){
+        image.addEventListener('click', function(e){
+            var imageGenres = e.target.dataset.genres;
+                if (clickCheck.includes(imageGenres)){
+                    e.target.style.borderColor = "black";
+                    clickCount -= 1;
+                    var index = genresSelected.indexOf(imageGenres);
+                    if (index > -1) {
+                        genresSelected.splice(index, 1);
+                        clickCheck.splice(index,1)
+                    }
+                }else{
+                    e.target.style.borderColor = "yellow";
+                    genresSelected.push(imageGenres);
+                    clickCheck.push(imageGenres);
+                    clickCount += 1
+                }
+                console.log(genresSelected);
+        });
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    imageSelect();
+});
+
+/*
+User clicks image
+Image is highlighted
+Image genre(s) added to array
+If clicked again, image is unhighlighted
+Image's genre(s) removed from array
+Limit to 3(?) total images clicked = Still need to implement this
+*/
