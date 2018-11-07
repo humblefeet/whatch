4c46ba86ab4d5d548a0c21c7833df1d9c07220e3
Sarah Stensberg <ladystensberg@gmail.com>
2018-11-06 16:51:12 -0800

    used justwatch module to search for movie

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const JustWatch = require('justwatch-api');
 const axios = require('axios');
 const base_url = 'https://api.themoviedb.org/3/'
 const api_key = `?api_key=${process.env.TMDB_API_KEY}`
@@ -10,7 +11,6 @@ module.exports = {
 		axios.get(`${base_url}discover/movie${api_key}&with_genres=${genres}`)
 			.then(function(response) {
 				var movies = response.data.results;
-				console.log(movies);
 				res.render('movies/index', {user, movies});
 			})
 	},
@@ -19,6 +19,24 @@ module.exports = {
 		axios.get(`${base_url}movie/${movie}${api_key}`)
 			.then(function(response) {
 				var movie = response.data;
+				var jw = new JustWatch();
+				jw.search({query: movie.title})
+					.then(function(results) {
+						var result = results.items[0];
+						var offers = result.offers;
+						console.log(offers);
+						var providers = [];
+						offers.forEach(function(provider) {
+							// if provider_id == 
+								// 8 - netflix
+								// 9 - amazon
+								// 10 - amazon-instant-video
+								// 15 - hulu
+								// 119 - amazon-prime-video
+							// same provider name to providers array and return result to res.render
+						})
+					})
+					
 				res.render('movies/show', {user: req.userModel, movie});
 			})
 	}
