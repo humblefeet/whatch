a90588b0b892b8079a88de26a55a7f6072bc271a
Sarah Stensberg <ladystensberg@gmail.com>
2018-11-08 12:47:33 -0800

    added popular movies to main index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+require('dotenv').config()
+const axios = require('axios');
+const base_url = 'https://api.themoviedb.org/3/'
+const api_key = `?api_key=${process.env.TMDB_API_KEY}`
 
 router.get('/', function(req, res) {
-	res.render('index', { user: req.user });
+	axios.get(`${base_url}movie/popular${api_key}&language=en-US&page=1`)
+	.then(function(response) {
+		var movies = response.data.results;
+		res.render('index', { user: req.user, movies });
+	})
 });
 
 router.get('/auth/google', passport.authenticate(
